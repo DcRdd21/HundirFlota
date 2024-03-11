0f913cd43fd547e116d0fc4ba4c565fcc539c2b0
DcRdd21 <95550502+DcRdd21@users.noreply.github.com>
2024-03-11 13:32:26 +0100

    Add files via upload

---
+++ src/HundirLaFlota/Barcos.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HundirLaFlota;
+
+/**
+ *
+ * @author cabdelda
+ */
+public class Barcos {
+    private static String nombre;
+    private static int longitud;
+    private static char id;
+
+    public Barcos() {
+    }
+
+    public static String getNombre() {
+        return nombre;
+    }
+
+    public static void setNombre(String nombre) {
+        Barcos.nombre = nombre;
+    }
+
+    public static int getLongitud() {
+        return longitud;
+    }
+
+    public static void setLongitud(int longitud) {
+        Barcos.longitud = longitud;
+    }
+
+    public static char getId() {
+        return id;
+    }
+
+    public static void setId(char id) {
+        Barcos.id = id;
+    }
+
+    
+
+        
+    
+   
+    
+    
+}
