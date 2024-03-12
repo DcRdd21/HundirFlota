9ad7e9cc0da4200ab211f9c09769bc991e322183
DcRdd21 <95550502+DcRdd21@users.noreply.github.com>
2024-03-12 13:51:48 +0100

    Add files via upload

---
+++ HundirLaFlota/Portaaviones.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HundirLaFlota;
+
+/**
+ *
+ * @author cabdelda
+ */
+public class Portaaviones extends Barcos{
+
+    public Portaaviones() {
+        super("portaaviones","p",5,false);
+    }
+    
+    
+}
---
+++ HundirLaFlota/Tablero.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HundirLaFlota;
+
+/**
+ *
+ * @author cabdelda
+ */
+public class Tablero {
+
+public static void rellenarTablero(char[][] tablero) {
+    for (int i = 0; i < tablero.length; i++) {
+        for (int j = 0; j < tablero[i].length; j++) {
+            // Rellenar cada fila con su valor
+            tablero[i][j] = 'X';
+            System.out.print(tablero[i][j] + " "); // construccion del tablero
+        }
+        System.out.println(); // imprimir tras cada iteracion
+    }
+}
+  
+       
+  
+
+    
+}
